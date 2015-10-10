427389e569c5a4f772d9ee2a738d3ec89fccb390
Ashish Baghudana <ashish.baghudana26@gmail.com>
2015-10-10 17:52:13 +0200

    Updated convert.py

--- convert.py
+++ convert.py
@@ -2,10 +2,11 @@ from pubtator import PubTator2Anndoc
 
 # Define entity classes here
 # entity_classes = {'Gene': 'e_1', 'FamilyName': 'e_2', 'DomainMotif': 'e_3', 'Species': 'e_4'}
-entity_class = {}
+entity_class =
 
 # Define input file
 input_file = '.'
 
 pub2anndoc = PubTator2Anndoc(entity_classes)
 pub2anndoc.parse(input_file)
+# Use pub2anndoc.parse(input_file, output_dir) if you want the output directory to be different than the input directory.
